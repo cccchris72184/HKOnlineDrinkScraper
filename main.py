a16ecc0a0113d2e275ec051c5260aae561ec00d3
cccchris72184 <59962810+cccchris72184@users.noreply.github.com>
2024-08-19 23:14:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pns_script
 import wellcome_script
 import hktvmall
+import subprocess
 
-if __name__ == '__main__':
-    pns_script.run() 
-    wellcome_script.run()  
-    hktvmall.run()  
+if __name__ == '__main':
+    subprocess.run(['python', 'pns_script.py'])
+    subprocess.run(['python', 'wellcome_script.py'])
+    subprocess.run(['python', 'hktvmall.py'])
